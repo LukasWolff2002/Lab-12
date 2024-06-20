15f9ede2ccbae07eb7c6b41ff5812d6dfbf5185d
LukasWolff2002 <lwolff@miuandes.cl>
2024-06-20 16:58:37 -0400

    avance

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,6 +82,9 @@ Rails.application.configure do
   # require "syslog/logger"
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new "app-name")
 
+  config.secret_key_base = Rails.application.credentials.secret_key_base
+
+
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
